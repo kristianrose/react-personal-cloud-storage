eb6b6a3e6f9a219fd48d32dadf2269781721c0eb
Serhii Sharipov <sharipov14@gmail.com>
2024-02-23 09:25:18 -0500

    Fix Home button layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,30 @@ import { mainScreenshot } from "../assets";
 import { CentredContainer } from "../components/auth/CentredContainer";
 import { AuthHeader } from "../components/auth/AuthHeader";
 
-export default function Dashboard() {
+export default function Home() {
   return (
-    <>
-      <CentredContainer>
-        <AuthHeader title="Easy access to your content" subtitle="Store files and folders from your mobile device, tablet, or computer" />
+    <CentredContainer>
+      <AuthHeader
+        title="Easy access to your content"
+        subtitle="Store files and folders from your mobile device, tablet, or computer"
+      />
 
-        <div className="flex flex-col items-center text-center">
-          <div className="card w-fit overflow-clip rounded-md bg-base-100 shadow-md">
-            <img className="h-80 w-fit" src={mainScreenshot} />
-          </div>
-          <div className="card w-40"></div>
-
-          <div className="mt-6 flex w-full gap-4">
-            <Link className="btn btn-primary grow" to="/login">
-              Login
-            </Link>
-
-            <Link className="btn btn-primary grow" to="/signup">
-              Sign Up
-            </Link>
-          </div>
+      <div className="flex flex-col items-center text-center">
+        <div className="card w-fit overflow-clip rounded-md bg-base-100 shadow-md">
+          <img className="h-80 w-fit" src={mainScreenshot} />
         </div>
-      </CentredContainer>
-    </>
+        <div className="card w-40"></div>
+
+        <div className="mt-6 flex w-full gap-4">
+          <Link className="btn btn-primary w-full shrink" to="/login">
+            Login
+          </Link>
+
+          <Link className="btn btn-primary w-full shrink" to="/signup">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </CentredContainer>
   );
 }
