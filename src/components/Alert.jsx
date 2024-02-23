6d411fe8c95d3293919da5fc01dbec492b1bf4ad
Serhii Sharipov <sharipov14@gmail.com>
2024-02-23 10:03:27 -0500

    Fix Alert icon

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -4,7 +4,11 @@ export function Alert({ alert }) {
   return (
     <div className="toast toast-center toast-middle z-50 w-screen max-w-lg">
       <div className={"alert flex text-white " + alert.className} role="alert">
-        {alert.className === "alert-success" ? <CheckCircle2 /> : <XCircle />}
+        {alert.className === "alert-success" ? (
+          <CheckCircle2 className="shrink-0" />
+        ) : (
+          <XCircle className="shrink-0" />
+        )}
         <span className="flex-grow text-wrap text-center ">
           {alert.message}
         </span>
