23cf534d6afa2620284ecc829e6155553a9e67dd
Serhii Sharipov <sharipov14@gmail.com>
2024-02-23 09:24:16 -0500

    Fix CentredContainer

--- src/components/auth/CentredContainer.jsx
+++ src/components/auth/CentredContainer.jsx
@@ -1,7 +1,7 @@
 export function CentredContainer(props) {
   return (
-    <div className="flex h-full max-h-dvh w-full flex-col justify-center bg-white md:bg-slate-200 md:py-3">
-      <div className="m-auto flex h-fit w-svw max-w-lg flex-col overflow-auto bg-base-100 p-4 md:card md:p-6 md:shadow-xl ">
+    <div className="flex h-full w-full justify-center bg-white md:bg-slate-200 md:py-3">
+      <div className="m-auto flex h-fit w-full max-w-lg flex-col bg-base-100 p-4 md:card md:p-6 md:shadow-xl">
         {props.children}
       </div>
     </div>
